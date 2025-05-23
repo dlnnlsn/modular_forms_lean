e0f641131150ed5690be884847edad30151d558b
Dylan Nelson <me@dylannelson.co.za>
2025-05-23 12:01:01 +0200

    show that eta is differentiable on the upper half plane

--- ModularForms/EtaFunction.lean
+++ ModularForms/EtaFunction.lean
@@ -1,5 +1,12 @@
+import Mathlib.Analysis.Calculus.Deriv.Mul
+import Mathlib.Analysis.Calculus.FDeriv.Basic
+import Mathlib.Analysis.Calculus.FDeriv.Comp
+import Mathlib.Analysis.Calculus.FDeriv.Mul
+import Mathlib.Analysis.Complex.LocallyUniformLimit
 import Mathlib.Analysis.Complex.UpperHalfPlane.Basic
 import Mathlib.Analysis.Complex.UpperHalfPlane.MoebiusAction
+import Mathlib.Analysis.Complex.UpperHalfPlane.Topology
+import Mathlib.Analysis.SpecialFunctions.ExpDeriv
 import Mathlib.Analysis.SpecialFunctions.Pow.Complex
 import Mathlib.Analysis.SpecificLimits.Basic
 import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
@@ -60,4 +67,33 @@ lemma multipliableLocallyUniformlyOn_eta_product :
   exact le_of_lt pi_pos
   rw [Set.not_nonempty_iff_eq_empty.mp hK_nonempty]
   exact tendstoUniformlyOn_empty
+
+lemma eta_differentiableOn_upperHalfPlane :
+    DifferentiableOn ℂ (eta ∘ UpperHalfPlane.ofComplex) { τ : ℂ | 0 < τ.im } := by
+  suffices DifferentiableOn ℂ (fun z : ℂ ↦ cexp (π * Complex.I * z / 12) *
+    ∏' n : ℕ, (1 - cexp (2 * π * Complex.I * ↑(n + 1) * z))) {z : ℂ | 0 < z.im } by
+      apply DifferentiableOn.congr this
+      intro z hz
+      lift z to ℍ using hz
+      apply comp_ofComplex
+  apply DifferentiableOn.mul
+  apply Differentiable.differentiableOn
+  apply Differentiable.comp Complex.differentiable_exp 
+  apply Differentiable.div_const
+  apply Differentiable.const_mul _
+  rw [show (fun y : ℂ ↦ y) = id by rfl]
+  exact differentiable_id
+  apply TendstoLocallyUniformlyOn.differentiableOn
+    multipliableLocallyUniformlyOn_eta_product.hasProdLocallyUniformlyOn
+  filter_upwards with s τ hτ
+  apply DifferentiableWithinAt.finset_prod
+  intro n hn
+  apply DifferentiableAt.differentiableWithinAt
+  apply Differentiable.differentiableAt
+  apply Differentiable.const_sub
+  apply Differentiable.comp Complex.differentiable_exp
+  apply Differentiable.const_mul
+  rw [show (fun y : ℂ ↦ y) = id by rfl]
+  exact differentiable_id
+  exact isOpen_lt continuous_const continuous_im
 
